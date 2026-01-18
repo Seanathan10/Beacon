e5823b7e9d7e51cb99597ccdc7311d436327d6e7
iconsumeplutonium <uzymc242@outlook.com>
2026-01-18 03:14:09 -0800

    embed svgs into jsx so it loads faster

--- Frontend/src/components/LocationPin.tsx
+++ Frontend/src/components/LocationPin.tsx
@@ -12,18 +12,46 @@ interface LocationPinProps {
 }
 
 function HeartIcon({ filled }: { filled: boolean }) {
-	return (
-		<img
-			src={filled ? '/filledheart.svg' : '/outlineheart.svg'}
-		/>
+	return filled ? (
+		<svg xmlns="http://www.w3.org/2000/svg" height="24px" viewBox="0 0 24 24" width="24px"
+			fill="#4db688">
+			<path d="M0 0h24v24H0V0z" fill="none" />
+			<path
+				d="M12 21.35l-1.45-1.32C5.4 15.36 2 12.28 2 8.5 2 5.42 4.42 3 7.5 3c1.74 0 3.41.81 4.5 2.09C13.09 3.81 14.76 3 16.5 3 19.58 3 22 5.42 22 8.5c0 3.78-3.4 6.86-8.55 11.54L12 21.35z" />
+		</svg>
+	) : (
+		<svg xmlns="http://www.w3.org/2000/svg" height="24px" viewBox="0 0 24 24" width="24px"
+			fill="#1f1f1f">
+			<path d="M0 0h24v24H0V0z" fill="none" />
+			<path
+				d="M16.5 3c-1.74 0-3.41.81-4.5 2.09C10.91 3.81 9.24 3 7.5 3 4.42 3 2 5.42 2 8.5c0 3.78 3.4 6.86 8.55 11.54L12 21.35l1.45-1.32C18.6 15.36 22 12.28 22 8.5 22 5.42 19.58 3 16.5 3zm-4.4 15.55l-.1.1-.1-.1C7.14 14.24 4 11.39 4 8.5 4 6.5 5.5 5 7.5 5c1.54 0 3.04.99 3.57 2.36h1.87C13.46 5.99 14.96 5 16.5 5c2 0 3.5 1.5 3.5 3.5 0 2.89-3.14 5.74-7.9 10.05z" />
+		</svg>
 	)
 }
 
 function BookmarkIcon({ filled }: { filled: boolean }) {
+	return filled ? (
+		<svg xmlns="http://www.w3.org/2000/svg" height="24px" viewBox="0 0 24 24" width="24px"
+			fill="#4db688">
+			<path d="M0 0h24v24H0V0z" fill="none" />
+			<path d="M17 3H7c-1.1 0-2 .9-2 2v16l7-3 7 3V5c0-1.1-.9-2-2-2z" />
+		</svg>
+	) : (
+		<svg xmlns="http://www.w3.org/2000/svg" height="24px" viewBox="0 0 24 24" width="24px"
+			fill="#1f1f1f">
+			<path d="M0 0h24v24H0V0z" fill="none" />
+			<path d="M17 3H7c-1.1 0-2 .9-2 2v16l7-3 7 3V5c0-1.1-.9-2-2-2zm0 15l-5-2.18L7 18V5h10v13z" />
+		</svg>
+	)
+}
+
+function InfoIcon() {
 	return (
-		<img
-			src={filled ? '/bookmark.svg' : '/bookmarkoutline.svg'}
-		/>
+		<svg xmlns="http://www.w3.org/2000/svg" height="24px" viewBox="0 0 24 24" width="24px" fill="#1f1f1f">
+			<path d="M0 0h24v24H0V0z" fill="none" />
+			<path
+				d="M11 7h2v2h-2zm0 4h2v6h-2zm1-9C6.48 2 2 6.48 2 12s4.48 10 10 10 10-4.48 10-10S17.52 2 12 2zm0 18c-4.41 0-8-3.59-8-8s3.59-8 8-8 8 3.59 8 8-3.59 8-8 8z" />
+		</svg>
 	)
 }
 
@@ -170,13 +198,11 @@ export default function LocationPin({ selectedPoint, setSelectedPoint, onShowDet
 					<button
 						className="location-popup-button"
 						onClick={onShowDetails}
-						style={{ background: 'none', padding: '4px 8px', }}
+						style={{ background: 'none', padding: '4px 8px', transform: 'translateY(3px)'}}
 						onMouseEnter={(e) => (e.currentTarget.style.opacity = "0.9")}
 						onMouseLeave={(e) => (e.currentTarget.style.opacity = "1")}
 					>
-						<img
-							src={'/info.svg'}
-						/>
+						<InfoIcon />
 					</button>
 
 					<button
